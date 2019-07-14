19057a733697723c6ee0692a84bc6e2a3ff3087e
Gogs <gogs@fake.local>
2019-07-14 17:14:19 -0300

    Incluido a implementacao da classe no controller

--- Tarefas/Controllers/TarefasController.cs
+++ Tarefas/Controllers/TarefasController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Tarefas.Models;
+using Tarefas.Services;
 
 namespace Tarefas.Controllers
 {
@@ -12,9 +13,15 @@ namespace Tarefas.Controllers
     {
         // lista de tarefas
         public IActionResult Index()
-        {
-            // obter os dados e retornar
-            return View();
+        {            
+            // obter os dados e retornar            
+            //TempTarefaItemService servico = new TempTarefaItemService();
+            //var tarefas = servico.GetItemAsync();
+            //return View(tarefas);
+
+            // obter os dados e retornar            
+
+            return View(tarefas);            
         }
     }
 }
